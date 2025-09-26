043e890366f53cf58bff2a77827f9b27678a0c79
West6879 <west13731@gmail.com>
2025-09-26 18:21:01 -0400

    Shift de arreglo en O(n^2)

--- Tareas/src/practicas/ShiftDeArreglo.java
+++ Tareas/src/practicas/ShiftDeArreglo.java
@@ -1,7 +1,6 @@
 package practicas;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class ShiftDeArreglo {
 
@@ -15,34 +14,49 @@ public class ShiftDeArreglo {
         lista.add("F");
         lista.add("G");
         lista.add("H");
-        shift(lista, 5);
-        System.out.println(lista);
+
+        System.out.println("Inicio:" + lista + "\n");
+        shift(lista, 6);
+        System.out.println("\nFinal:" + lista);
     }
 
-    public static void swap(ArrayList<String> lista, int i, int j) {
-        String temp =  lista.get(i);
-        lista.set(i, lista.get(j));
-        lista.set(j, temp);
+    /*
+        Funcion para rotar subarreglos del arreglo, cada llamada rota una vez hacia la derecha.
+    */
+    public static void rotar(ArrayList<String> lista, int inicio, int fin) {
+        String ultimo = lista.get(fin);
+        for(int i = fin; i > inicio; i--) {
+            lista.set(i, lista.get(i - 1));
+        }
+        lista.set(inicio, ultimo);
+        System.out.println(lista.subList(inicio, fin + 1)); // Descomentar si desea ver el proceso de rotación
     }
 
+    // T tiene que estar entre 1 y n-1, osea no puede ser mayor que el tamaño del array
     public static void shift(ArrayList<String> lista, int T) {
-        if(lista.size() % 2 != 0 || T > lista.size() - 1) return;
+        if(lista.size() % 2 != 0) return;
+        if(T > lista.size() - 1) return; // Descomentar esta linea si desea T > n-1
         int mitad_1, mitad_2;
-        int medio = lista.size() / 2;
+        int medio = lista.size() / 2; // Medio del array
+
         if(T % 2 == 0) {
             mitad_1 = mitad_2 = T/2;
         } else {
             mitad_1 = (T/2) + 1;
             mitad_2 = T/2;
         }
-        for (int i = medio - T; i > 0; i--) {
-            swap(lista, i, (i + mitad_1) % (lista.size()/2));
+
+        // Rotacion de la primera mitad
+        for(int i = 0; i < mitad_1; i++) {
+            rotar(lista, 0, medio - 1);
         }
-        /*
-        for(int i = 0; i < mitad_2; i++) {
-            swap(lista, i + (lista.size() / 2), (i + (lista.size() / 2) + mitad_2) % (lista.size()));
+
+        System.out.println();
+
+        // Rotacion de la segunda mitad
+        for(int j = 0; j < mitad_2; j++) {
+            rotar(lista, medio, lista.size() - 1);
         }
-        */
     }
 
 }
